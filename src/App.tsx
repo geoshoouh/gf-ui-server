f139ad9ccd9bfd31f1144f15d55eecec14854b28
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-11 09:40:54 -0600

    add correct path for auth server

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import StatusIndicator from './components/StatusIndicator'
 function App() {
 
   // Dev Value; must be on local network
-  const authServerEndpoint: string = "http://rpi2:31772"
+  const authServerEndpoint: string = "http://rpi2:31772/ping"
   
   return (
     <>
