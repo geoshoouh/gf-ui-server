f45458841069bd784b19ccb1c68894dc820c4831
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-11 09:37:09 -0600

    use dev value for endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import './App.css'
 import StatusIndicator from './components/StatusIndicator'
 
 function App() {
+
+  // Dev Value; must be on local network
+  const authServerEndpoint: string = "http://rpi2:31772"
   
   return (
     <>
       <div className="card">
-        <StatusIndicator subject="Login Server"/>
+        <StatusIndicator endpoint={authServerEndpoint} subject="Login Server"/>
       </div>
     </>
   )
