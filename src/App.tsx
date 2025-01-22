aaae877cffe33eb86c8af5e4dfc0ede23a7f3a3c
geoshoouh <joshua.d.tate12@gmail.com>
2025-01-21 19:55:56 -0600

    add route for ping

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
 
   const [appUser, setAppUser] = useState<GPF_User>({role: '', token: '', isAuthenticated: false})
 
-  const authServerEndpoint: string = "https://authentication.gfproto.xyz"
+  const authServerEndpoint: string = "https://app.gfproto.xyz"
   
   return (
     <>
